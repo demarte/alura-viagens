7e9ce35120e4b037bae0b20db1f50663bad4f7e8
Ivan De Martino <ivan_demartino@outlook.com>
2019-03-07 17:06:05 -0300

    (#2) Configuracao das celulas e da tabela a partir dos dados do servidor

--- Alura Viagens/Controllers/ViewController.swift
+++ Alura Viagens/Controllers/ViewController.swift
@@ -9,48 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    // MARK: - IBOutlets
-    
-    @IBOutlet weak var tabelaViagens: UITableView!
-    @IBOutlet weak var viewHoteis: UIView!
-    @IBOutlet weak var viewPacotes: UIView!
-    
-    // MARK: - Atributos
-    
-    let listaViagens:[Viagem] = ViagemDao().retornaTodasAsViagens()
-    
-    // MARK: - View Life Cycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configuraViews()
+
+  // MARK: - IBOutlets
+
+  @IBOutlet weak var tabelaViagens: UITableView!
+  @IBOutlet weak var viewHoteis: UIView!
+  @IBOutlet weak var viewPacotes: UIView!
+
+  // MARK: - Atributos
+
+  var listaViagens:[Viagem]?
+
+  // MARK: - View Life Cycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configuraViews()
+    fetchViagens()
+  }
+
+  // MARK: - Métodos
+
+  private func configuraViews() {
+    viewPacotes.layer.cornerRadius = 10
+    viewHoteis.layer.cornerRadius = 10
+  }
+
+  private func fetchViagens() {
+    APIViagensService().fetchViagens { (viagens) in
+      switch viagens {
+      case .success(let value):
+        self.listaViagens = value
+        DispatchQueue.main.async {
+          self.tabelaViagens.reloadData()
+        }
+        break
+      case .failure(_):
+        break
+      }
     }
-    
-    // MARK: - Métodos
-    
-    func configuraViews() {
-        viewPacotes.layer.cornerRadius = 10
-        viewHoteis.layer.cornerRadius = 10
+  }
+
+  // MARK: - UITableViewDataSource
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return listaViagens?.count ?? 0
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+    if let viagens = listaViagens {
+      let viagemAtual = viagens[indexPath.row]
+      cell.configuraCelula(viagemAtual)
     }
-    
-    // MARK: - UITableViewDataSource
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listaViagens.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
-        let viagemAtual = listaViagens[indexPath.row]
-        cell.configuraCelula(viagemAtual)
-        
-        return cell
-    }
-    
-    // MARK: - UITableViewDelegate
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? 175 : 260
-    }
+    return cell
+  }
+
+  // MARK: - UITableViewDelegate
+
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? 175 : 260
+  }
 }

cbce9faa40f531c778c8c6df97e067f8365c531f
Ivan De Martino <ivan_demartino@outlook.com>
2019-03-07 17:06:58 -0300

    (#2) Alteracao dos models conforme a API e o portocol Codable

--- Alura Viagens/Models/PacoteViagem.swift
+++ Alura Viagens/Models/PacoteViagem.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class PacoteViagem: NSObject {
-    
-    let nomeDoHotel: String
-    let descricao: String
-    let dataViagem: String
-    let viagem: Viagem
-    
-    init(nomeDoHotel: String, descricao: String, dataViagem: String, viagem: Viagem) {
-        self.nomeDoHotel = nomeDoHotel
-        self.descricao = descricao
-        self.dataViagem = dataViagem
-        self.viagem = viagem
-    }
+struct PacoteViagem: Codable {
 
+  let id: Int
+  let titulo: String
+  let quantidadeDeDias: Int
+  let preco: String
+  let caminhoDaImagem: String
+  let localizacao: String
+  let nomeDoHotel: String
+  let servico: String
+  let dataViagem: String
+
+  enum CodingKeys: String, CodingKey {
+    case id, titulo, quantidadeDeDias, preco, localizacao, nomeDoHotel, servico
+    case dataViagem = "data"
+    case caminhoDaImagem = "imageUrl"
+  }
 }

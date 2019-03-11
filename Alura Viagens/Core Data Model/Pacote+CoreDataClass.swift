474c4a5cbe9baef88f65d018cbf95f2ba8b6e23e
Ivan De Martino <ivan_demartino@outlook.com>
2019-03-11 03:58:45 -0300

    (#4) Criacao do model do Core Data

---
+++ Alura Viagens/Core Data Model/Pacote+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Pacote+CoreDataClass.swift
+//  Alura Viagens
+//
+//  Created by Ivan De Martino on 3/9/19.
+//  Copyright © 2019 Alura. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Pacote: NSManagedObject {
+
+}
---
+++ Alura Viagens/Core Data Model/Pacote+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Pacote+CoreDataProperties.swift
+//  Alura Viagens
+//
+//  Created by Ivan De Martino on 3/11/19.
+//  Copyright © 2019 Alura. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Pacote {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Pacote> {
+        return NSFetchRequest<Pacote>(entityName: "Pacote")
+    }
+
+    @NSManaged public var caminhoDaImagem: String
+    @NSManaged public var preco: String
+    @NSManaged public var quantidadeDeDias: Int16
+    @NSManaged public var servico: String
+    @NSManaged public var titulo: String
+    @NSManaged public var dataViagem: String
+
+}

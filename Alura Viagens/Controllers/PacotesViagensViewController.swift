8c29a1b8f22cc41719e2c6bdc8c7e2f081307303
Ivan De Martino <ivan_demartino@outlook.com>
2019-04-25 15:44:09 -0300

    (#17) Metodo de delegate para lidar com as transicoes de vc

--- Alura Viagens/Controllers/PacotesViagensViewController.swift
+++ Alura Viagens/Controllers/PacotesViagensViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PacotesViagensViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UISearchBarDelegate {
+class PacotesViagensViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UISearchBarDelegate, UINavigationControllerDelegate {
 
   // MARK: - IBOutlets
 
@@ -21,11 +21,14 @@ class PacotesViagensViewController: UIViewController, UICollectionViewDataSource
   var listaViagens: [PacoteViagem] = []
   var listaFavoritos: [PacoteViagem] = []
   let persistenceService = PersistenceService.shared
+  var pacoteSelecionado: PacoteViagem?
+  var frameSelecionado: CGRect?
 
   // MARK: - View Life Cycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    navigationController?.delegate = self
     pesquisarViagens.delegate = self
     fetchPacotesViagensAPI()
   }
@@ -96,9 +99,14 @@ class PacotesViagensViewController: UIViewController, UICollectionViewDataSource
 
   func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     let pacote = listaViagens[indexPath.item]
+    pacoteSelecionado = pacote
     let storyboard = UIStoryboard(name: "Main", bundle: nil)
     let controller = storyboard.instantiateViewController(withIdentifier: "detalhes") as! DetalhesViagemViewController
     controller.pacoteSelecionado = pacote
+
+    let atributosItemSelecionado = collectionView.layoutAttributesForItem(at: indexPath)
+    frameSelecionado = atributosItemSelecionado?.frame
+
     navigationController?.pushViewController(controller, animated: true)
   }
 
@@ -106,5 +114,31 @@ class PacotesViagensViewController: UIViewController, UICollectionViewDataSource
 
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     return UIDevice.current.userInterfaceIdiom == .phone ? CGSize(width: collectionView.bounds.width/2-20, height: 160) : CGSize(width: collectionView.bounds.width/3-20, height: 250)
+  }
+
+  // MARK: - UINavigationControllerDelegate
+
+  func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+
+    guard let caminhoDaImagem = pacoteSelecionado?.id else { return nil}
+    guard let imagem = UIImage(named: "\(caminhoDaImagem)") else { return nil}
+    guard let frameInicial =  frameSelecionado else { return nil }
+
+
+    switch operation {
+    case .push:
+      return AnimacaoTransicaoPersonalizada(
+        duracao: TimeInterval(UINavigationController.hideShowBarDuration),
+        imagem: imagem,
+        frameInicial: frameInicial,
+        apresentarViewController: true)
+      
+    default:
+      return AnimacaoTransicaoPersonalizada(
+        duracao: TimeInterval(UINavigationController.hideShowBarDuration),
+        imagem: imagem,
+        frameInicial: frameInicial,
+        apresentarViewController: false)
+    }
   }
 }
